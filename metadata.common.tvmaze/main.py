f839e86d7789fe9d679e647d02b189f9b29b0669
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2019-08-28 13:15:52 +0300

    Remove debugging code

--- metadata.common.tvmaze/main.py
+++ metadata.common.tvmaze/main.py
@@ -18,8 +18,6 @@
 from __future__ import absolute_import
 import sys
 from libs.actions import router
-from web_pdb import catch_post_mortem
 
 if __name__ == '__main__':
-    with catch_post_mortem():
-        router(router(sys.argv[2][1:]))
+    router(sys.argv[2][1:])
