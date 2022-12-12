ed61324aec19e858fd17cd860d195ba440b242b3
Roman <roman1972@gmail.com>
2022-12-12 09:22:20 +0200

    Remove the comment that is no longer relevant

--- metadata.tvmaze/libs/actions.py
+++ metadata.tvmaze/libs/actions.py
@@ -116,9 +116,6 @@ def get_episode_list(episodeguide: str, episode_order: str) -> None:  # pylint: 
             logger.error(f'Unable to determine TVmaze show ID from episodeguide: {episodeguide}')
             return
     if show_id is None and not episodeguide.isdigit():
-        # Kodi has a bug: when a show directory contains an XML NFO file with
-        # episodeguide URL, that URL is always passed here regardless of
-        # the actual parsing result in get_show_from_nfo()
         logger.warning(f'Invalid episodeguide format: {episodeguide} (probably URL).')
         show_id = data_service.parse_url_episodeguide(episodeguide)
     if show_id is None and episodeguide.isdigit():
